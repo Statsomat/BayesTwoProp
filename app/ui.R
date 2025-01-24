54fe86b595416ea38763fcd90fe08d9128567bbb
Denise Welsch <denise.welsch@reyar.de>
2025-01-24 14:41:36 +0000

    drop logo

--- app/ui.R
+++ app/ui.R
@@ -62,9 +62,7 @@ shinyUI(fluidPage(
   tags$div(
     style = "display: flex; justify-content: space-between; align-items: center; padding: 10px;",
     # Left logo
-    a(img(src='Logo.jpg', width=200), href="https://www.statsomat.com", target="_blank"),
-    # Right logo
-    a(img(src='HSKoblenz.png', width=200), href="https://www.hs-koblenz.de/home", target="_blank")
+    a(img(src='Logo.jpg', width=200), href="https://www.statsomat.com", target="_blank")
   ),
   
   
